cf23cfe228dc316eed2671efcfc891d08827df26
David Rachwalik <DeadlyStraw@hotmail.com>
2017-10-20 12:30:33 -0500

    Completed tutorial step 5 of 7, services

--- AngularToH/AngularToH/ClientApp/app/app.module.shared.ts
+++ AngularToH/AngularToH/ClientApp/app/app.module.shared.ts
@@ -10,12 +10,15 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
+        HeroDetailComponent,
         HomeComponent
     ],
     imports: [
